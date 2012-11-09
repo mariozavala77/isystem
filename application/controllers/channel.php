bfbf3b2ef0e1c8e7e826c549eaaea99356f96225
shaoqi <shaoqisq123@gmail.com>
2012-11-09 16:26:40 +0800

    渠道管理，api控制器

---
+++ application/controllers/channel.php
@@ -0,0 +1,102 @@
+<?php
+
+/**
+ * 渠道控制器
+ *
+ * @author: shaoqi <413729863@qq.com>
+ * @copyright: Copyright (c) 2012 UFCEC Tech All Rights Reserved.
+ * @version: $Id$
+ */
+class Channel_Controller extends Base_Controller {
+
+    // 渠道默认页
+    public function action_index(){
+        $types = Config::get('application.channel_type');
+
+        return View::make('channel.list', array('channel_type' => $types));
+    }
+
+    // 渠道列表
+    public function action_filter(){
+        $fields = ['type', 'name', 'alias', 'synced_at','id'];
+        $channel = Channel::filter($fields);
+        $data = Datatables::of($channel)->make();
+
+        return Response::json($data);
+    }
+
+    // 渠道添加
+    public function action_add(){
+        $type = Input::get('type'); // 获取分类
+        $types = Config::get('application.channel_type'); // 获取允许的渠道分类
+
+        if (!isset($types[$type])){
+            session::flash('tips', '渠道分类不存在');
+
+            return Redirect::back();
+        }
+        return View::make('channel.' . strtolower($type) . '_add', array('channel' => $types[$type]));
+    }
+
+    // 插入渠道信息
+    public function action_insert(){
+        $data = [
+            'type' => Input::get('type'),
+            'name' => Input::get('name'),
+            'alias' => Input::get('alias'),
+            'description'  => Input::get('description'),
+            'accredit' => serialize(Input::get('accredit')),
+            'synced_at' => date('Y-m-d H:i:s')
+        ];
+
+        if(Channel::insert( $data )) {
+            session::flash('tips', sprintf('渠道%s添加成功！', $data['type']) );
+
+            return Redirect::to('channel');
+        } else {
+            session::flash('tips', '添加失败。');
+
+            return Redirect::back();
+        }
+    }
+
+    // 渠道编辑
+    public function action_edit(){
+        $channel_id = Input::get('channel_id');
+        $channel = Channel::info($channel_id);
+        $channel->accredit = unserialize($channel->accredit);
+        $types = Config::get('application.channel_type');
+        return View::make('channel.' . strtolower($channel->type) . '_edit', array('channel' => $channel, 'channel_type' => $types));
+    }
+
+    // 更新渠道信息
+    public function action_update(){
+        $channel_id = Input::get('channel_id');
+
+        $data = [
+            'name'    => Input::get('name'),
+            'alias'      => Input::get('alias'),
+            'description'      => Input::get('description'),
+            'accredit'    => serialize(Input::get('accredit')),
+        ];
+
+        Channel::update($channel_id, $data);
+
+        return Redirect::to('channel');
+    }
+
+    // 删除渠道
+    public function action_delete(){
+        $channel_id = Input::get('channelid');
+
+        $channel_id = intval($channel_id);
+
+        if (empty($channel_id)){
+            return Response::json([ 'status' => 'error', 'message' => '渠道id无效']);
+        }
+
+        $response = Channel::delete($channel_id);
+
+        return $response ? Response::json([ 'status' => 'success', 'message' => '删除成功！']) :Response::json([ 'status' => 'error', 'message' => '删除失败！']);
+    }
+}
---
+++ application/views/channel/agent_edit.blade.php
@@ -0,0 +1,90 @@
+@layout('layout.base')
+@section('script')
+@endsection
+@section('sidebar')
+@include('block.sidebar');
+@endsection
+@section('content')
+<!-- COntent begins -->
+<div id="content">
+    <div class="contentTop">
+        <span class="pageTitle"><span class="icon-user-2"></span>编辑{{ $channel_type[$channel->type]['cn'] }}渠道</span>
+        <div class="clear"></div>
+    </div>
+
+    <!-- Breadcrumbs line begins -->
+    <div class="breadLine">
+        <div class="bc">
+            <ul id="breadcrumbs" class="breadcrumbs">
+                <li><a href="{{ URL::base() }}">控制中心</a></li>
+                <li><a href="{{ URL::base() }}/channel" title="渠道管理">渠道管理</a></li>
+                <li class="current"><a href="{{ URL::base() }}/channel/edit?channel_id={{ $channel->id }}" title="编辑{{ $channel_type[$channel->type]['cn'] }}渠道">编辑{{ $channel_type[$channel->type]['cn'] }}渠道-{{ $channel->alias }}</a></li>
+            </ul>
+        </div>
+
+        <div class="breadLinks">
+            <ul>
+                <li><a href="#" title=""><i class="icos-list"></i><span>新订单</span> <strong>(+58)</strong></a></li>
+                <li><a href="#" title=""><i class="icos-check"></i><span>新任务</span> <strong>(+12)</strong></a></li>
+                <li class="has">
+                    <a title="">
+                        <i class="icos-money3"></i>
+                        <span>快捷导航</span>
+                        <span><img src="/images/elements/control/hasddArrow.png" alt=""></span>
+                    </a>
+                    <ul>
+                        <li><a href="#" title=""><span class="icos-add"></span>New invoice</a></li>
+                        <li><a href="#" title=""><span class="icos-archive"></span>History</a></li>
+                        <li class="noBorderB"><a href="#" title=""><span class="icos-printer"></span>Print invoices</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <div class="clear"></div>
+        </div>
+    </div>
+    <!-- Breadcrumbs line ends -->
+
+
+    <!-- Main content bigins -->
+    <div class="wrapper">
+        <form action="{{ URL::base() }}/channel/update" method="POST" class="main">
+            <fieldset>
+                <div class="widget fluid">
+                    <div class="whead"><h6>编辑{{ $channel_type[$channel->type]['cn'] }}渠道</h6><div class="clear"></div></div>
+                    <div class="formRow">
+                        <div class="grid3"><label>渠道名称：</label></div>
+                        <div class="grid9"><input type="text" name="name" style="width: 50%" required="true" value="{{$channel->name}}"/></div>
+                        <div class="clear"></div>
+                    </div>
+                    <div class="formRow">
+                        <div class="grid3"><label>渠道别名：</label></div>
+                        <div class="grid9"><input type="text" name="alias" style="width: 50%" required="true" value="{{$channel->alias}}"/></div>
+                        <div class="clear"></div>
+                    </div>
+                    <div class="formRow">
+                        <div class="grid3"><label>渠道许可密钥：</label></div>
+                        <div class="grid9"><input type="text" name="accredit['secret']" style="width: 30%" required="true" value="{{$channel->accredit['secret']}}"/></div>
+                        <div class="clear"></div>
+                    </div>
+                    <div class="formRow">
+                        <div class="grid3"><label>服务请求地址：</label></div>
+                        <div class="grid9"><input type="text" name="accredit['Server']" style="width: 50%" required="true" value="{{$channel->accredit['Server']}}"/></div>
+                        <div class="clear"></div>
+                    </div>
+                    <div class="formRow">
+                        <div class="grid3"><label>渠道描述：</label></div>
+                        <div class="grid9"><textarea rows="8" cols="" name="description" class="auto lim">{{$channel->description}}</textarea></div>
+                        <div class="clear"></div>
+                    </div>
+                    <div class="formRow textC">
+                        <input type="hidden" name="channel_id" value="{{ $channel->id }}">
+                        <span><input type="submit" class="bBlue buttonM" value="保存" /></span>
+                    </div>
+                </div>
+            </fieldset>
+        </form>
+    </div>
+    <!-- Main content ends -->
+
+</div>
+@endsection
