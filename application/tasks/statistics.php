ddf8c99c9a8915cb948fcf5e17dd033d4aea3a76
shaoqi <shaoqisq123@gmail.com>
2012-12-10 18:09:47 +0800

    订单统计,以及计划任务

---
+++ application/tasks/statistics.php
@@ -0,0 +1,137 @@
+<?php
+/**
+ * 统计任务
+ *
+ */
+class Statistics_Task extends Task{
+    
+    public function run( $arguments = [] ){
+        if(empty($arguments)){
+            $this->_handle();
+            exit;
+        }
+
+        $command = (isset($arguments[0]) && $arguments[0] !=='') ? $arguments[0] : 'help';
+        $args = array_slice($arguments, 1);
+
+        switch ($command) {
+            case 'purchasing':
+                # code...
+                break;
+            case 'help':
+                $this->_help();
+            break;
+            default:
+                $this->_handle();
+                break;
+        }
+    }
+
+    /**
+     * 帮助
+     */
+    private function _help(){
+        $html = ['统计任务帮助 ：'];
+        array_push($html, 'purchasing'."\t".'新增订单统计');
+        array_push($html, 'purchasing'."\t".'新增订单统计');
+        array_push($html, 'purchasing'."\t".'新增订单统计');
+        echo implode("\r\n\t", $html);
+    }
+
+    private function _handle(){
+        //var_dump($this->_day('20121010'));
+        $purchased_at = Order::get_purchased_at();
+        $updated_at = Order::get_updated_at();
+        $null = '0000-00-00 00:00:00';
+        if ($purchased_at['min'] == $null && $updated_at['min'] != $null)
+        {
+            $min = $updated_at['min'];
+        }
+
+        if($purchased_at['min'] != $null && $updated_at['min'] == $null)
+        {
+            $min = $purchased_at['min'];
+        }
+        if($purchased_at['min'] != $null && $updated_at['min'] != $null)
+        {
+            $min = min($purchased_at['min'], $updated_at['min']);
+        }
+        if ($purchased_at['max'] == $null && $updated_at['max'] != $null)
+        {
+            $max = $updated_at['max'];
+        }
+
+        if ($purchased_at['max'] != $null && $updated_at['max'] == $null)
+        {
+            $max = $purchased_at['max'];
+        }
+        if ($purchased_at['max'] != $null && $updated_at['max'] != $null)
+        {
+            $max = max($purchased_at['max'], $updated_at['max']);
+        }
+        $min = substr($min,0,10);
+        $statistics_date = Statistics::max_date();
+        $min = $statistics_date?max($statistics_date, $min):$min;
+        $max = substr($max,0,10);
+
+        if ($min == $max)
+        {
+            $this->_day($min);
+        }
+        else
+        {
+            $min_time = strtotime($min);
+            $max_time = strtotime($max);
+            $day = ($max_time-$min_time)/86400;
+            for ($i=0; $i <= $day; $i++) 
+            {
+                $times = $min_time+$i*86400;
+                $this->_day($times);
+            }
+        }
+    }
+
+    private function _day($times){
+        $date = date('Y-m-d', $times);
+        $statistics_purchased = Order::statistics_purchased($date);
+        $statistics_purchased = $statistics_purchased[0];
+        if (!empty($statistics_purchased->price))
+        {
+            $statistics['purchased'] = $statistics_purchased->total;
+            $statistics['purchased_price'] = $statistics_purchased->price;
+        }
+
+        $statistics_status = Order::statistics_status($date);
+
+        if (!empty($statistics_status))
+        {
+            $field = [1 => 'pending', 2 => 'unshipped', 3 => 'shipped', 
+                      4 => 'partial_shipment', 5 => 'canceled', 6 => 'unfulfillable'];
+            
+            foreach ($statistics_status as $value) 
+            {
+                $key = $field[$value->status];
+                $statistics[$key] = $value->total;
+                $statistics[$key . '_price'] = $value->price;        
+            }
+        }
+
+        if (empty($statistics))
+        {
+            return false;
+        }
+
+        $id = date('Ymd', $times);
+
+        if (Statistics::exists($id))
+        {
+            Statistics::update($id, $statistics);
+        }
+        else
+        {
+            $statistics['id'] = $id;
+            $statistics['date'] = $date;
+            Statistics::insert($statistics);
+        }
+    }
+}
--- application/views/statistics/main.blade.php
+++ application/views/statistics/main.blade.php
@@ -8,55 +8,6 @@
 @section('content')
 <!-- Content begins --> 
 <script type="text/javascript">
-var slibe;
-var order_data=[
-    {date:'11-01',order:4321,pay:3786,refund:1,warehousing:5434},
-    {date:'11-02',order:3423,pay:3128,refund:1,warehousing:4324},
-    {date:'11-03',order:4324,pay:4126,refund:1,warehousing:5354},
-    {date:'11-04',order:3455,pay:3126,refund:1,warehousing:4232},
-    {date:'11-05',order:5432,pay:5234,refund:1,warehousing:3493},
-    {date:'11-06',order:4324,pay:4127,refund:1,warehousing:6453},
-    {date:'11-07',order:6545,pay:6454,refund:1,warehousing:5456},
-    {date:'11-08',order:3213,pay:3121,refund:1,warehousing:6753},
-    {date:'11-09',order:4353,pay:4324,refund:1,warehousing:4353},
-    {date:'11-10',order:5643,pay:5324,refund:1,warehousing:5467},
-    {date:'11-11',order:4323,pay:3787,refund:1,warehousing:5765},
-    {date:'11-12',order:4532,pay:4234,refund:1,warehousing:4543},
-    {date:'11-13',order:4398,pay:3789,refund:1,warehousing:4645},
-    {date:'11-14',order:4324,pay:3545,refund:1,warehousing:4547},
-    {date:'11-15',order:3233,pay:2335,refund:1,warehousing:3543},
-    {date:'11-16',order:3243,pay:2344,refund:0,warehousing:4522},
-    {date:'11-17',order:4565,pay:3454,refund:1,warehousing:2434},
-    {date:'11-18',order:4323,pay:3234,refund:1,warehousing:4631},
-    {date:'11-19',order:6543,pay:5472,refund:1,warehousing:6432},
-    {date:'11-20',order:3893,pay:3214,refund:1,warehousing:3453},
-    {date:'11-21',order:4675,pay:4332,refund:1,warehousing:4532},
-    {date:'11-22',order:6784,pay:4554,refund:1,warehousing:5433},
-    {date:'11-23',order:7432,pay:5734,refund:1,warehousing:6314},
-    {date:'11-24',order:8904,pay:8452,refund:1,warehousing:7454},
-    {date:'11-25',order:9435,pay:8563,refund:1,warehousing:6543},
-    {date:'11-26',order:8954,pay:8324,refund:1,warehousing:9234},
-    {date:'11-27',order:9932,pay:9534,refund:1,warehousing:11325},
-    {date:'11-28',order:11245,pay:10879,refund:1,warehousing:12657},
-    {date:'11-29',order:4321,pay:3678,refund:1,warehousing:4532},
-];
-    var sales_region_Data = [{
-                country: "US",
-                visits: Math.round(Math.random() * 600)+2132,
-                color: "#FF0F00"
-            }, {
-                country: "GB",
-                visits: Math.round(Math.random() * 600)+1822,
-                color: "#F8FF01"
-            },{
-                country: "CA",
-                visits: Math.round(Math.random() * 600)+674,
-                color: "#CD0D74"
-            },{
-                country: "NO",
-                visits: Math.round(Math.random() * 600)+89,
-                color: "#0D52D1"
-            }];
             var product_class_statistics_Data = [{
                 year: '充电器',
                 income: Math.round(Math.random() * 600)+3000,
@@ -79,14 +30,14 @@ var order_data=[
                 color: "#0D52D1"
             }];
             AmCharts.ready(function () {
-                sales_region(sales_region_Data);
+                get_sales_region();
                 product_class_statistics(product_class_statistics_Data);
-                orders_chart();
+                get_orders('', '');
             });
 function sales_region(chartData){
                 var chart = new AmCharts.AmSerialChart();
                 chart.dataProvider = chartData;
-                chart.categoryField = "country";
+                chart.categoryField = "shipping_country";
                 chart.startDuration = 1;
 
                 // AXES
@@ -107,7 +58,7 @@ function sales_region(chartData){
 
                 // GRAPH
                 var graph = new AmCharts.AmGraph();
-                graph.valueField = "visits";
+                graph.valueField = "price";
                 graph.colorField = "color";
                 graph.balloonText = "[[category]]: [[value]]";
                 graph.type = "column";
@@ -156,8 +107,8 @@ function product_class_statistics(chartData){
                 chart.write("product_class_statistics");
 }
 
-function orders_chart() {
-    slibe = new AmCharts.AmSerialChart();
+function orders_chart(order_data) {
+    var slibe = new AmCharts.AmSerialChart();
     slibe.dataProvider = order_data;
     slibe.categoryField = "date";
     slibe.balloon.color = "#000000";
@@ -184,7 +135,7 @@ function orders_chart() {
     // GRAPH
     var graph = new AmCharts.AmGraph();
     graph.title = "当日订单累计金额";
-    graph.valueField = "order";
+    graph.valueField = "purchased_price";
     graph.balloonText = "当日订单累计金额[[value]]";
     graph.hideBulletsCount = 40;
     graph.lineAlpha = 1;
@@ -192,7 +143,7 @@ function orders_chart() {
     slibe.addGraph(graph);
     var graph = new AmCharts.AmGraph();
     graph.title = "当日支付累计金额支付量";
-    graph.valueField = "pay";
+    graph.valueField = "unshipped_price";
     graph.balloonText = "当日支付累计金额[[value]]";
     graph.hideBulletsCount = 20;
     graph.lineAlpha = 1;
@@ -200,7 +151,7 @@ function orders_chart() {
     slibe.addGraph(graph);
     var graph = new AmCharts.AmGraph();
     graph.title = "当日退款累计金额";
-    graph.valueField = "refund";
+    graph.valueField = "unfulfillable_price";
     graph.balloonText = "当日退款累计金额[[value]]";
     graph.hideBulletsCount = 20;
     graph.lineAlpha = 1;
@@ -208,7 +159,7 @@ function orders_chart() {
     slibe.addGraph(graph);
     var graph = new AmCharts.AmGraph();
     graph.title = "当日出库累计金额";
-    graph.valueField = "warehousing";
+    graph.valueField = "shipped_price";
     graph.balloonText = "当日出库累计金额[[value]]";
     graph.hideBulletsCount = 20;
     graph.lineAlpha = 1;
@@ -226,12 +177,35 @@ function orders_chart() {
     slibe.write("orders");
 }
 $(function() {
-    $('#fromDate, #toDate').datepicker({
+    $('#fromDate,#toDate').datepicker({
         changeMonth: false,
         showOtherMonths:true,
         numberOfMonths: 1,
+        dateFormat: "yy-mm-dd",
+        maxDate:-1,
     });
 });
+function search(){
+    var from = $('#fromDate').val();
+    var to = $('#toDate').val();
+    from = from?from:to;
+    to = to?to:from;
+    if(!from && !to){
+        $.jGrowl('请选择筛选的日期');
+        return false;
+    }
+    get_orders(from, to);
+}
+function get_orders(from, to){
+    $.getJSON('/statistics/day',{start:from,end:to},function(data){
+        orders_chart(data);
+    });
+}
+function get_sales_region(){
+    $.getJSON('/statistics/region',function(data){
+        sales_region(data);
+    });
+}
 </script>   
 <div id="content">
     <div class="contentTop">
@@ -272,16 +246,17 @@ $(function() {
         <div class="widget">
                 <!--产品每日动态 包括 下单量，支付量，退款量，出库量 货物的金额-->
                 <div class="whead"><h6>销售动态</h6><div class="clear"></div></div>
-                <!--<div class="formRow">
-                        <div class="grid3"><label>Dates range:</label></div>
-                        <div class="grid9">
-                            <ul class="datesRange">
-                                <li><input type="text" id="fromDate" name="from" placeholder="From" title="开始时间"/></li>
-                                <li class="sep">-</li>
-                                <li><input type="text" id="toDate" name="to" placeholder="To" title="结束时间"/></li>
-                            </ul>
-                        </div><div class="clear"></div>
-                    </div>-->
+                <div class="formRow">
+                    <div class="grid3"><label>时间检索:</label></div>
+                    <div class="grid9">
+                        <ul class="datesRange">
+                            <li><input type="text" id="fromDate" name="from" placeholder="From" title="开始时间" readonly="readonly"/></li>
+                            <li class="sep">-</li>
+                            <li><input type="text" id="toDate" name="to" placeholder="To" title="结束时间" readonly="readonly"/></li>
+                            <li><input type="button" value="search" onclick="search()"></li>
+                        </ul>
+                    </div><div class="clear"></div>
+                </div>
                 <div class="chart" id="orders"></div>
         </div>
     </div>
